94c5a18c61fa7731720689e3743d627f7b94471c
sajib689 <jibonraja11@gmail.com>
2024-09-05 18:16:46 +0600

    add credential

--- src/app/api/auth/[...nextauth]/route.jsx
+++ src/app/api/auth/[...nextauth]/route.jsx
@@ -1,10 +1,26 @@
-import NextAuth from 'next/next-auth'
+import NextAuth from "next-auth/next";
+import  CredentialsProvider from 'next-auth/providers/credentials';
 
 const handler = NextAuth({
     session: {
         strategy: 'jwt',
     },
     providers: [
-        
+        CredentialsProvider({
+            credentials: {
+                email: {label: 'Email', type: 'text', required: true, placeholder: 'Enter your email'},
+                password: {label: 'Password', type: 'password', required: true, placeholder: 'Enter your password'},
+
+            },
+            async authorize (credentials) {
+                if(!credentials){
+                    return null;
+                } else {
+                    return true;
+                }
+            }
+        })
     ]
 })
+
+export {handler as GET, handler as POST}
